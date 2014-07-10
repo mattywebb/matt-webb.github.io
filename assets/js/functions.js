2d465d27cf9cf07638905523f6345e7e3bb45d77
mark-squad <mark@madebyprimal.co>
2014-07-10 14:23:47 +1000

    Pinpoint slideshow error

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,15 +1,17 @@
 
-/* trigger when page is ready */
-$(document).ready(function (){
 
-	// SVG -> PNG Fallback through Modernizer
-	if(!Modernizr.svg) {
-	    $('img[src*="svg"]').attr('src', function() {
-	        return $(this).attr('src').replace('.svg', '.png');
-	    });
-	}
+$(window).load(function() {
+    
+
+    var greetings = [ "hello", "ciao", "welcome", "howdy", "greetings", "salut", "hallo", "hola", "Gday", "Hey"];
+	var greeting_id = Math.floor(Math.random() * greetings.length);
+	document.getElementById('title').innerHTML = greetings[greeting_id];
+
 	
-	
+	var images = ["matt-red.jpg", "matt-green.jpg", "matt-yellow.jpg", "matt-blue.jpg", "matt-orange.jpg"];
+	var dir = 'assets/img/';
+	var images_id = Math.floor(Math.random() * images.length);
+	document.getElementById('header-cover').style.backgroundImage = "url(" + dir + images[images_id] + ")";
     
 });
 	
