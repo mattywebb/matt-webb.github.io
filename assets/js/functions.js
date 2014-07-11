9b4e657aa77782c9c2e955fb0d74d1ccdcaa1338
Matt Webb <hello@mattwebb.com.au>
2014-07-11 12:01:35 +1000

    Added Dribbble Feed

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -77,6 +77,7 @@ $(document).ready(function(){
 	
 	}
 	
+	//INSTAFEED
 	var feed = new Instafeed({
         get: 'user',
         userId: 19458322,
@@ -86,8 +87,19 @@ $(document).ready(function(){
         template: '<li><img src="{{image}}" /></li>'
     });
     feed.run();
-	    
-
+    
+    //JRIBBBLE
+    // API Ref: http://api.dribbble/players/:id/shots
+	$.jribbble.getShotsByPlayerId('lukestevenson', function (playerShots) {
+	var html = [];
+	$.each(playerShots.shots, function (i, shot) {
+	html.push('<div class="shot"><a href="' + shot.url + '" target="_blank">');
+	html.push('<img class="shot-image" src="' + shot.image_url + '" ');
+	html.push('alt="' + shot.title + '"></a></div>');
+	});
+	
+	$('#shotsByPlayerId').html(html.join(''));
+	}, {page: 1, per_page: 3});
 
 
     var greetings = [ "hello", "ciao", "welcome", "howdy", "greetings", "salut", "hallo", "hola", "Gday", "Hey"];
